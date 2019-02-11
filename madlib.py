98b20d5d4408c1fd540908b546a3c0be4c315e36
Zack Nadrotowicz <zachary.nadrotowicz1@marist.edu>
2019-02-11 17:31:13 -0500

    Solution to the Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,8 @@
+# madlib.py
+# Writes a Mad Lib
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+place = input("Enter a place: ")
+print ("Move your " + adjective + " " + noun + " and " + verb + " it in the " + place + "!")
