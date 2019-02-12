c4567c5ff880609dde58030245618116a39f2e52
znadrotowicz34 <47338013+znadrotowicz34@users.noreply.github.com>
2019-02-11 21:10:09 -0500

    Added main function

--- madlib.py
+++ madlib.py
@@ -1,8 +1,10 @@
 # madlib.py
 # Writes a Mad Lib
 
-noun = input("Enter a noun: ")
-verb = input("Enter a verb: ")
-adjective = input("Enter an adjective: ")
-place = input("Enter a place: ")
-print ("Move your " + adjective + " " + noun + " and " + verb + " it in the " + place + "!")
+def main():
+  noun = input("Enter a noun: ")
+  verb = input("Enter a verb: ")
+  adjective = input("Enter an adjective: ")
+  place = input("Enter a place: ")
+  print ("Move your " + adjective + " " + noun + " and " + verb + " it in the " + place + "!")
+main()
