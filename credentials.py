7bd06c1ef082ff2f95d2f3ef179ac2036e2603e6
Zack Nadrotowicz <zachary.nadrotowicz1@marist.edu>
2019-02-25 17:38:09 -0500

    Created functions for different variables

--- credentials.py
+++ credentials.py
@@ -2,21 +2,34 @@
 # Lab #5 – Working with Strings and Functions
 # Author: Zachary Nadrotowicz
 # Created: 2019-02-25
-def main():
     # get user's first and last names
+
+
+def getName():
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
+    return [first,last]
     # TODO modify this to generate a Marist-style username
-    uname = first + "." + last
+def maristUname(name):
+    uname = name[0] + "." + name[1]
+    return uname
     # ask user to create a new password
+def getPasswd():
     passwd = input("Create a new password: ")
     # TODO modify this to ensure the password has at least 8 characters
-    while True:
-        if len(passwd) < 8:
-           print("Fool of a Took! That password is feeble!")
-           passwd = input("Create a new password: ")
-        elif len(passwd) >= 8:
-            print("The force is strong in this one…")
-            print("Account configured. Your new email address is " + uname + "@marist.edu")
-            break            
-main()
+    while not len(passwd) >= 8:
+         if len(passwd) < 8:
+            print("Fool of a Took! That password is feeble!")
+            passwd = input("Create a new password: ")  
+    return passwd
+          
+                       
+
+def main():
+    name = getName()
+    uname = maristUname(name)
+    passwd = getPasswd()
+    print("The force is strong in this one…")
+    print("Account configured. Your new email address is " + uname + "@marist.edu")
+main()
+
