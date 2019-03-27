345c98bd3ea1d638e9b7aaf34f23e665215fb02e
Zack Nadrotowicz <zachary.nadrotowicz1@marist.edu>
2019-03-27 10:09:44 -0400

    Usability improvments to the guessing game, resolves #4 and #5.

--- guessing-game.py
+++ guessing-game.py
@@ -6,12 +6,19 @@ def main():
     animalCorrect = 0
 
     while animalCorrect == 0:
-        animal = input("I am thinking of an animal. What animal am I thinking of?")
+        animal = input("I am thinking of an animal. What animal am I thinking of?" )
         answer = animal.lower()
         if answer == "panda":
             print("Correct!")
             animalCorrect = 1
-        elif answer == "Quit" or answer == "quit":
+            likeness = input("Do you like this animal? ")
+            if likeness == "yes":
+                print("It is a cool animal!")
+            elif likeness == "no":
+                print("I understand not all animals are liked.")
+            else:
+                print("I guess I'll never know if you like pandas or not")
+        elif answer[0] == "q":
             print("You have quit the game.")
             animalCorrect = 1
         else:
